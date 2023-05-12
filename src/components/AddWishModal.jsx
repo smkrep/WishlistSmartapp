4d71f0274f2cb30e58385ac71d60a6c8ab76f53a
Rodeet <58859443+Rodeet@users.noreply.github.com>
2023-05-12 19:14:53 +0300

    pudge

--- src/components/AddWishModal.jsx
+++ src/components/AddWishModal.jsx
@@ -6,7 +6,16 @@ export const AddWishModal = ({active, setActive}) => {
   return (
     <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
         <div className='modal__content' onClick={e => e.stopPropagation()}>
+          <button className="closeModal" onClick={() => setActive(false)}>×</button>
             <form>
+
+                <select className="selector" id="importance" name="importance">
+                    <option value="important">Важно</option>
+                    <option value="medium">Средней важности</option>
+                    <option value="not-important">Неважно</option>
+                </select>
+
+                <br />
                 
                 <input className="textInput" type="text" id="name" name="name" placeholder="Введите название" />
 
@@ -18,21 +27,16 @@ export const AddWishModal = ({active, setActive}) => {
                 <br />
 
                 
-                <select className="selector" id="importance" name="importance">
-                    <option value="important">Важно</option>
-                    <option value="medium">Средней важности</option>
-                    <option value="not-important">Неважно</option>
-                </select>
-
-                <br />
-
-                
                 <input className="textInput" type="text" id="category" name="category" placeholder="Введите категорию" />
 
                 <br />
 
                 
                 <textarea className="textArea" id="additional-info" name="additional-info"></textarea>
+
+                <br />
+
+                <button className="buttonSubmit" type="submit">Добавить</button>
 
             </form>
         </div>
