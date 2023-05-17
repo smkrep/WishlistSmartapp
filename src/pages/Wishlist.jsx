7e733f2eff4db44a687721d021517d6d57b4abba
smkrep <m2101760@edu.misis.ru>
2023-05-17 19:44:12 +0300

    started implementing voice assistance

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -2,7 +2,22 @@ import React, {useState} from 'react'
 import Wish from '../components/Wish'
 import { AddWishButton } from '../components/AddWishButton'
 import { AddWishModal } from '../components/AddWishModal'
+// import {
+//   createSmartappDebugger,
+//   createAssistant,
+// } from "@salutejs/client";
 
+
+// const initializeAssistant = (getState/*: any*/) => {
+//   if (process.env.NODE_ENV === "development") {
+//     return createSmartappDebugger({
+//       token: process.env.REACT_APP_TOKEN ?? "",
+//       initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
+//       getState,
+//     });
+//   }
+//   return createAssistant({ getState });
+// };
 
 export default class WishList extends React.Component {
   constructor (props){
@@ -11,7 +26,54 @@ export default class WishList extends React.Component {
     this.state = {
       wishes: []
     }
+
+    // this.assistant = initializeAssistant(() => this.getStateForAssistant() );
+    // this.assistant.on("data", (event/*: any*/) => {
+    //   console.log(`assistant.on(data)`, event);
+    //   const { action } = event
+    //   this.dispatchAssistantAction(action);
+    // });
+    // this.assistant.on("start", (event) => {
+    //   console.log(`assistant.on(start)`, event);
+    // });
+
   }
+
+  // getStateForAssistant () {
+  //   console.log('getStateForAssistant: this.state:', this.state)
+  //   const state = {
+  //     item_selector: {
+  //       items: this.state.notes.map(
+  //         ({ id, title }, index) => ({
+  //           number: index + 1,
+  //           id,
+  //           title,
+  //         })
+  //       ),
+  //     },
+  //   };
+  //   console.log('getStateForAssistant: state:', state)
+  //   return state;
+  // }
+
+  // dispatchAssistantAction (action) {
+  //   console.log('dispatchAssistantAction', action);
+  //   if (action) {
+  //     switch (action.type) {
+  //       case 'add_note':
+  //         return this.add_note(action);
+
+  //       case 'done_note':
+  //         return this.done_note(action);
+
+  //       case 'delete_note':
+  //         return this.delete_note(action);
+
+  //       default:
+  //         throw new Error();
+  //     }
+  //   }
+  // }
 
   onAddChild = (state) => {
     this.setState({
