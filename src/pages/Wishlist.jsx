61573836d470b00c58e6094bd57d860e804f0a88
smkrep <m2101760@edu.misis.ru>
2023-05-18 00:14:57 +0300

    voice addition implementation

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -2,22 +2,22 @@ import React, {useState} from 'react'
 import Wish from '../components/Wish'
 import { AddWishButton } from '../components/AddWishButton'
 import { AddWishModal } from '../components/AddWishModal'
-// import {
-//   createSmartappDebugger,
-//   createAssistant,
-// } from "@salutejs/client";
+import {
+  createSmartappDebugger,
+  createAssistant,
+} from "@salutejs/client";
 
 
-// const initializeAssistant = (getState/*: any*/) => {
-//   if (process.env.NODE_ENV === "development") {
-//     return createSmartappDebugger({
-//       token: process.env.REACT_APP_TOKEN ?? "",
-//       initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
-//       getState,
-//     });
-//   }
-//   return createAssistant({ getState });
-// };
+const initializeAssistant = (getState/*: any*/) => {
+  if (process.env.NODE_ENV === "development") {
+    return createSmartappDebugger({
+      token: process.env.REACT_APP_TOKEN ?? "",
+      initPhrase: `Запусти ${process.env.REACT_APP_SMARTAPP}`,
+      getState,
+    });
+  }
+  return createAssistant({ getState });
+};
 
 export default class WishList extends React.Component {
   constructor (props){
@@ -27,53 +27,62 @@ export default class WishList extends React.Component {
       wishes: []
     }
 
-    // this.assistant = initializeAssistant(() => this.getStateForAssistant() );
-    // this.assistant.on("data", (event/*: any*/) => {
-    //   console.log(`assistant.on(data)`, event);
-    //   const { action } = event
-    //   this.dispatchAssistantAction(action);
-    // });
-    // this.assistant.on("start", (event) => {
-    //   console.log(`assistant.on(start)`, event);
-    // });
+    this.wishobj = {importance: 'important', name: '', price: '', category: '', additional_info: ''}
+
+    this.assistant = initializeAssistant(() => this.getStateForAssistant() );
+    this.assistant.on("data", (event/*: any*/) => {
+      //console.log(`assistant.on(data)`, event);
+      const { action } = event
+      this.dispatchAssistantAction(action);
+    });
+    this.assistant.on("start", (event) => {
+      //console.log(`assistant.on(start)`, event);
+    });
 
   }
 
-  // getStateForAssistant () {
-  //   console.log('getStateForAssistant: this.state:', this.state)
-  //   const state = {
-  //     item_selector: {
-  //       items: this.state.notes.map(
-  //         ({ id, title }, index) => ({
-  //           number: index + 1,
-  //           id,
-  //           title,
-  //         })
-  //       ),
-  //     },
-  //   };
-  //   console.log('getStateForAssistant: state:', state)
-  //   return state;
-  // }
+  getStateForAssistant () {
+    //console.log('getStateForAssistant: this.state:', this.state)
+    const state = {
+      item_selector: {
+        items: this.state.wishes.map(
+          ({ id, title }, index) => ({
+            number: index + 1,
+            id,
+            title,
+          })
+        ),
+      },
+    };
+    //console.log('getStateForAssistant: state:', state)
+    return state;
+  }
 
-  // dispatchAssistantAction (action) {
-  //   console.log('dispatchAssistantAction', action);
-  //   if (action) {
-  //     switch (action.type) {
-  //       case 'add_note':
-  //         return this.add_note(action);
-
-  //       case 'done_note':
-  //         return this.done_note(action);
-
-  //       case 'delete_note':
-  //         return this.delete_note(action);
-
-  //       default:
-  //         throw new Error();
-  //     }
-  //   }
-  // }
+  dispatchAssistantAction (action) {
+    //console.log('dispatchAssistantAction', action);
+    if (action) {
+      switch (action.type) {
+        case 'name':
+          this.wishobj.name = action.note
+          break
+        case 'importance':
+          //this.wishobj({priority: action.note})
+          break
+        case 'price':
+          this.wishobj.price = action.note
+          break
+        case 'category':
+          this.wishobj.category = action.note
+          break
+        case 'additional':
+          this.wishobj.additional_info = action.note
+          this.setState({
+            wishes: [...this.state.wishes, this.wishobj]
+          });
+          break
+      }
+    }
+  }
 
   onAddChild = (state) => {
     this.setState({
@@ -88,6 +97,9 @@ export default class WishList extends React.Component {
   }
 
   render() {
+    
+          
+
     const chA = [];
 
     for (var i = 0; i < this.state.wishes.length; i += 1) {
